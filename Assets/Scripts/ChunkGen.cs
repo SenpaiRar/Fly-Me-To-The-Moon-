3ba13e0a879af4f79c7372cf4f7f92c5ba935f77
SenpaiRar <windowsbeb@gmail.com>
2017-05-14 13:33:24 -0400

    Started on "Chunks" which are populated with pickup items

---
+++ Assets/Scripts/ChunkGen.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChunkGen : MonoBehaviour {
+
+    class Chunk
+    {
+        public Chunk(GameObject Parent, float STARTh, float ENDh, float w)
+        {
+            ParentObject = Parent;
+            StartingHeight = STARTh;
+            EndingHeight = ENDh;
+            Width = w;
+        }
+
+        float StartingHeight;
+        float EndingHeight;
+        float Width;
+        public GameObject ParentObject;
+        List<GameObject> ObjectsWithin; 
+
+        public float GetHeight()
+        {
+            return (StartingHeight);
+        }
+        public float GetWidth()
+        {
+            return (Width);
+        }
+        public void AddToObject (GameObject x)
+        {
+            ObjectsWithin.Add(x);
+        }
+        public void DestroyEachObject()
+        {
+            foreach (var item in ObjectsWithin)
+            {
+                Destroy(item);
+                ObjectsWithin.Remove(item);
+            }
+        }
+        public void PopulateChunk (GameObject Population, int density)
+        {
+            for (int i = 0; i < density; i++)
+            {
+                Instantiate(Population, new Vector3(Random.Range(0,Width),Random.Range(StartingHeight,EndingHeight),0), Quaternion.identity);
+            }
+        }
+    }
+
+    List<Chunk> ChunkList = new List<Chunk>();
+    float ChunkHeight;
+    float ChunkWidth;
+    float CurrentStartHeight;
+    float CurrentEndHeight;
+    GameObject testGameObject;
+
+    void Awake()
+    {
+        CurrentStartHeight = 0;
+        CurrentEndHeight = ChunkHeight;
+    }
+    
+    void CreateChunk()
+    {
+        GameObject temp = new GameObject();
+        temp.name = "Chunk #" + ChunkList.Count;
+        Chunk thisChunk = new Chunk(temp, CurrentStartHeight, CurrentEndHeight, ChunkWidth);
+        ChunkList.Add(thisChunk);
+
+        CurrentStartHeight += ChunkHeight;
+        CurrentEndHeight += ChunkHeight;
+    }
+    
+}
---
+++ Assets/Scripts/Rocket_Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket_Player : MonoBehaviour {
+    //Class handles the rocket and how the player controls it
+
+    public Rigidbody RocketBody;
+    public float RocketUPForce;
+    public float RocketSIDEForce;
+    
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RocketUpwards();
+        }
+
+        RocketSidewards(Input.GetAxisRaw("Horizontal"));
+	}
+
+    void RocketUpwards()//Adds force to the bottom of the craft
+    {
+        RocketBody.AddForce(0,RocketUPForce,0,ForceMode.Impulse);
+    }
+    
+    void RocketSidewards(float SideToMoveTo = 0) //SideToMoveTo = 0 will do nothing, 1 will move to right, -1 will move to the left
+    {
+        RocketBody.AddForce(RocketSIDEForce * SideToMoveTo, 0, 0,ForceMode.Force);
+    }
+}
